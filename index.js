14a877ab4a2ea4fb02ea2618eac39e071bdc9639
JauniKapauni <riches-goods.54@icloud.com>
2026-06-09 15:21:29 +0200

    feat: add login

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Loggeed in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
