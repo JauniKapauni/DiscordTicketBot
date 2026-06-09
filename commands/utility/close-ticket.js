a1be90f599ac61fa23928dad116655ce617c2ef7
JauniKapauni <riches-goods.54@icloud.com>
2026-06-09 19:28:23 +0200

    fix: wrong access of permission

--- commands/utility/close-ticket.js
+++ commands/utility/close-ticket.js
@@ -4,7 +4,7 @@ const db = require('../../database');
 module.exports = {
     data: new SlashCommandBuilder().setName('close-ticket').setDescription('Closes a ticket!'),
     async execute(interaction){
-        if(!interaction.channel.name.startsWith('ticket-') && !interaction.member.permission.has('Administrator')){
+        if(!interaction.channel.name.startsWith('ticket-') && !interaction.member.permissions.has('Administrator')){
             return interaction.reply({
                 content: 'This is not a ticket channel.',
                 ephemeral: true
