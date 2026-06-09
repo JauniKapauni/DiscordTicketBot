a31489789cfd9e555281ff573999cc6202d0d3f9
JauniKapauni <riches-goods.54@icloud.com>
2026-06-09 16:05:09 +0200

    feat: add permission flag

--- commands/utility/create-ticket.js
+++ commands/utility/create-ticket.js
@@ -1,11 +1,25 @@
-const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder().setName('create-ticket').setDescription('Creates a ticket!'),
     async execute(interaction){
         const channel = await interaction.guild.channels.create({
             name: `ticket-${interaction.user.username}`,
-            type: ChannelType.GuildText
+            type: ChannelType.GuildText,
+            permissionOverwrites: [
+                {
+                    id: interaction.guild.id,
+                    deny: [PermissionFlagsBits.ViewChannel]
+                },
+                {
+                    id: interaction.user.id,
+                    allow: [
+                        PermissionFlagsBits.ViewChannel,
+                        PermissionFlagsBits.SendMessages,
+                        PermissionFlagsBits.ReadMessageHistory
+                    ]
+                }
+            ]
         });
         await interaction.reply({
             content: `Ticket created: ${channel}`,
