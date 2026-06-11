08102c21e448311eda521b5d4bb90f57267b51a4
JauniKapauni <riches-goods.54@icloud.com>
2026-06-11 13:54:14 +0200

    fix: add perm check

--- commands/utility/create-panel.js
+++ commands/utility/create-panel.js
@@ -3,6 +3,12 @@ const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = re
 module.exports = {
 	data: new SlashCommandBuilder().setName('create-panel').setDescription('Creates ticket panel!'),
 	async execute(interaction){
+		if(!interaction.member.permissions.has('Administrator')){
+			return interaction.reply({
+				content: 'You do not have permission to use this command.',
+				ephemeral: true
+			});
+		}
 		const button = new ButtonBuilder().setCustomId('create-panel').setLabel('Create Ticket').setStyle(ButtonStyle.Success);
 		const actionRow = new ActionRowBuilder().addComponents(button);
 		await interaction.reply({
